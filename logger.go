94c1d0f86da5e85bc4e12e1297987f083ff225aa
Michael Stapelberg <michael@stapelberg.de>
2022-04-10 15:06:24 +0200

    update rsync.Logger doc

--- logger.go
+++ logger.go
@@ -2,5 +2,6 @@ package rsync
 
 import "github.com/gokrazy/rsync/internal/log"
 
-// Logger logs messages.
+// Logger is an interface that allows specifying your own logger.
+// By default, the Go log package is used, which prints to stderr.
 type Logger = log.Logger
