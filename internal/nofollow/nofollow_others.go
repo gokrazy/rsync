ab953f0689173cd796e6ef04ccbc298f841e9a69
Michael Stapelberg <michael@stapelberg.de>
2025-02-24 21:23:23 +0100

    remove os.Root TODO
    I looked into it and os.Root does not have all the operations we need.
    Specifically, as of Go 1.24, we need Readlink and Symlink.
    But even then, calls like unix.Mknod will likely never be part of os.Root.

--- internal/nofollow/nofollow_others.go
+++ internal/nofollow/nofollow_others.go
@@ -3,5 +3,5 @@
 package nofollow
 
 // Maybe resolves to unix.O_NOFOLLOW on unix systems,
-// 0 on other platforms. TODO(go1.24): use os.Root.
+// 0 on other platforms.
 const Maybe = 0
--- internal/nofollow/nofollow_unix.go
+++ internal/nofollow/nofollow_unix.go
@@ -5,5 +5,5 @@ package nofollow
 import "golang.org/x/sys/unix"
 
 // Maybe resolves to unix.O_NOFOLLOW on unix systems,
-// 0 on other platforms. TODO(go1.24): use os.Root.
+// 0 on other platforms.
 const Maybe = unix.O_NOFOLLOW
