a0bea6c1e155c8125fd6fc253a606c7788a40ef7
yjhi <yjhixy@163.com>
2025-12-26 16:21:14 +0800

    source directory has suffix will fail
    2025/12/26 15:17:20 rsyncos.go:32: remote protocol: 31
    2025/12/26 15:17:20 rsyncos.go:32: sender(paths=["C:\\Users\\yyy\\Desktop\\rsync\\111\\gg"])
    2025/12/26 15:17:20 do.go:43: SendFileList(modPath="\\\\?\\", paths=["C:\\Users\\yyy\\Desktop\\rsync\\111\\gg"])
    2025/12/26 15:17:20 flist.go:345: sendFileList()
    2025/12/26 15:17:20 flist.go:360:   path "C:\\Users\\yyy\\Desktop\\rsync\\111\\gg" (local dir "\\\\?\\")
    2025/12/26 15:17:20 flist.go:371:   filepath.Walk("\\\\?\\C:\\Users\\yyy\\Desktop\\rsync\\111\\gg"), strip="\\\\?\\C:\\Users\\yyy\\Desktop\\rsync\\111\\"
    2025/12/26 15:17:20 flist.go:372:   prefix="\\gg/"
    2025/12/26 15:17:20 flist.go:141: filepath.WalkFn(path=.)
    2025/12/26 15:17:20 flist.go:152: isDir=true, xferDirs=1
    2025/12/26 15:17:20 flist.go:162: Trim(path=".") = "\\gg/."
    2025/12/26 15:17:20 flist.go:141: filepath.WalkFn(path=11.txt)
    2025/12/26 15:17:20 flist.go:152: isDir=false, xferDirs=1
    2025/12/26 15:17:20 flist.go:162: Trim(path="11.txt") = "\\gg/11.txt"
    2025/12/26 15:17:20 do.go:51: file list sent
    2025/12/26 15:17:20 rsyncos.go:32: info: \gg/

--- internal/sender/flist.go
+++ internal/sender/flist.go
@@ -59,12 +59,19 @@ var (
 
 func getRootStrip(requested, localDir string) (string, string) {
 	root := filepath.Clean(filepath.Join(localDir, requested))
-
 	sep := string(os.PathSeparator)
-	strip := filepath.Dir(filepath.Clean(root)) + sep
-	if strings.HasSuffix(requested, sep) {
+
+	isEndSep := strings.HasSuffix(requested, "\\") || strings.HasSuffix(requested, "/")
+	strip := ""
+	if !isEndSep {
+		strip = filepath.Dir(filepath.Clean(root)) + sep
+		if strings.HasSuffix(requested, sep) {
+			strip = filepath.Clean(root) + sep
+		}
+	} else {
 		strip = filepath.Clean(root) + sep
 	}
+
 	return root, strip
 }
 
@@ -359,12 +366,19 @@ func (st *Transfer) SendFileList(localDir string, paths []string, excl *filterRu
 		if st.Opts.Verbose() { // TODO: DebugGTE(FLIST, 1)
 			st.Logger.Printf("  path %q (local dir %q)", requested, localDir)
 		}
+
 		// st.Logger.Printf("getRootStrip(requested=%q, localDir=%q", requested, localDir)
 		rootPath, strip := getRootStrip(requested, localDir)
 		// st.Logger.Printf("root=%q, strip=%q", root, strip)
 		prefix := strings.TrimPrefix(rootPath, filepath.Clean(strip))
 		if prefix != "" {
 			prefix = strings.TrimPrefix(prefix, "/")
+
+			//add by yjh
+			//此处会导致服务器路径带\
+			prefix = strings.TrimPrefix(prefix, "\\")
+			//
+
 			prefix += "/"
 		}
 		if st.Opts.Verbose() { // TODO: DebugGTE(FLIST, 1)
